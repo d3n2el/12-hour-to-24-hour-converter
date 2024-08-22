0cadf0353acfa2c18490f27e07f339359cf94e36
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-22 14:29:26 +0200

    Add files via upload

--- working.py
+++ working.py
@@ -8,20 +8,24 @@ def main():
 
 def convert(s):
     am,pm = s.split(" TO ")
-    match_am = re.search(r"[0-9]{1,2}:[0-9]{2} (AM|PM)", am)
-    match_pm = re.search(r"[0-9]{1,2}:[0-9]{2} (AM|PM)", pm)
-    if match_am and match_pm:
-        result_am = match_am.group()
-        result_pm = match_pm.group()
-        result_am = result_am.split(" ")
-        result_pm = result_pm.split(" ")
+    match_am = re.search(r"[0-9]{1,2}:?[0-9]{0,22} (AM|PM)", am)
+    match_pm = re.search(r"[0-9]{1,2}:?[0-9]{0,2} (AM|PM)", pm)
+    result_am = match_am.group()
+    result_pm = match_pm.group()
+    result_am = result_am.split(" ")
+    result_pm = result_pm.split(" ")
+    if ":" in  result_am[0] and ":" in  result_pm[0]:
         hours,minutes= result_pm[0].split(":")
         hours= int(hours)+12
+        minutes= int(minutes)
+        if minutes >= 60:
+            raise ValueError
         final_pm=f"{hours}:{minutes}"
-        print(final_pm)
-        return f"{result_am[0]} {result_pm[0]}"
+        return f"{result_am[0]} to {final_pm}"
     else:
-        print("No match found")
+        result_pm[0]= int(result_pm[0])+12
+        return f"{result_am[0]} to {result_pm[0]}"
+
 
 
 
