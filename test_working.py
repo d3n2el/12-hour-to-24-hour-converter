aefc7c63a13a15cf7e64f371b0daecd9210ba8de
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-22 21:10:11 +0200

    Add files via upload

--- test_working.py
+++ test_working.py
@@ -15,10 +15,17 @@ def test_valueError():
         convert("13:00 AM - 17:00 PM")
 
 def test_normal():
-    assert convert("9 AM TO 5 PM") == "09:00 to 17:00"
-    assert convert("9 am to 4 pm") == "09:00 to 16:00"
-    assert convert("12 am to 12 pm") == "00:00 to 12:00"
-    assert convert("11:23 am to 4:32 pm") == "01:23 to 16:32"
+    assert convert("12 AM TO 12 PM") == "00:00 to 12:00"
+    assert convert("10 AM TO 8 PM") == "10:00 to 20:00"
+    assert convert("11:23 am TO 4:32 pm") == "11:23 to 16:32"
 
-def test_
+def test_pmam():
+    assert convert("9 PM TO 5 AM") == "21:00 to 05:00"
+    assert convert("12 PM TO 2 AM") == "12:00 to 02:00"
+    assert convert("11:23 PM TO 3:34 AM") == "23:34 to 03:23"
+    assert convert("4:09 PM TO 5:06 AM") == "16:06 to 05:09"
+
+
+
+
 
