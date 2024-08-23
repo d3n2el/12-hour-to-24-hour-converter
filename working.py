5758586218d9f63e6b9e02360c8945f4c8f90d81
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-23 14:28:25 +0200

    Add files via upload

--- working.py
+++ working.py
@@ -3,74 +3,29 @@ import sys
 
 
 def main():
-    print(convert(input("Hours: ").upper()))
+    print(convert(input("Hours: ")))
 
 
 def convert(s):
-    am,pm = s.split(" TO ")
-    match_am = re.search(r"[0-9]{1,2}:?[0-9]{0,22} (AM|PM)", am)
-    match_pm = re.search(r"[0-9]{1,2}:?[0-9]{0,2} (AM|PM)", pm)
+    if not " to " in s:
+        raise ValueError
+    am,pm = s.split(" to ")
+    match_am = re.search(r"[0-9]{1,2}:?[0-9]{0,22}? (AM|PM)\b", am, re.IGNORECASE)
+    match_pm = re.search(r"[0-9]{1,2}:?[0-9]{0,2}? (AM|PM)\b", pm, re.IGNORECASE)
     if match_am == None or match_pm == None:
         raise ValueError
-    result_am = match_am.group()
-    result_pm = match_pm.group()
-    result_am = result_am.split(" ")
-    result_pm = result_pm.split(" ")
-    if ":" in  result_am[0] and ":" in  result_pm[0]:
-        if "PM" in result_am[1]:
-            hours_am,minutes_am= result_am[0].split(":")
-            hours_pm,minutes_pm= result_pm[0].split(":")
-            hours_pm= int(hours_pm)
-            minutes_pm= int(minutes_pm)
-            hours_am= int(hours_am)
-            minutes_am= int(minutes_am)
-            if minutes_pm >= 60 or hours_pm >=13 or hours_am >= 13 or minutes_am >= 60 :
-                raise ValueError
-            if hours_pm == 12:
-                hours_pm = 0
-            if hours_am == 12:
-                hours_am = 12
-            else:
-                hours_am= hours_am+12
-            final_am= f"{hours_pm:02d}:{minutes_am:02d}"
-            final_pm=f"{hours_am:02d}:{minutes_pm:02d}"
-            return f"{final_pm} to {final_am}"
+    match_am= match_am.group()
+    match_pm= match_pm.group()
+    find_am= re.search(r"^AM$", mathc_am)
+    find_pm= re.search(r"^PM$", mathc_pm)
+    if find_am and find_pm:
+        results_am= re.split(r"\s", match_am)
+        results_pm= re.split(r"\s", match_pm)
+        final_pm= int(results_pm[0])+ 12
+        result_am = int(results_am[0])
+        return f"{result_am:02d}:00 to {final_pm:02d}:00"
 
 
-        hours_am,minutes_am= result_am[0].split(":")
-        hours_pm,minutes_pm= result_pm[0].split(":")
-        hours_pm= int(hours_pm)
-        minutes_pm= int(minutes_pm)
-        hours_am= int(hours_am)
-        minutes_am= int(minutes_am)
-        if minutes_pm >= 60 or hours_pm >=13 or hours_am >= 13 or minutes_am >= 60 :
-            raise ValueError
-        if hours_am == 12:
-            hours_am = 0
-        if hours_pm == 12:
-            hours_pm = 12
-        else:
-            hours_pm= hours_pm+12
-        final_am= f"{hours_am:02d}:{minutes_am:02d}"
-        final_pm=f"{hours_pm:02d}:{minutes_pm:02d}"
-        return f"{final_am} to {final_pm}"
-    else:
-        if "PM" in result_am[1]:
-            result_pm[0]= int(result_pm[0])
-            result_am[0] = int(result_am[0])+12
-            if result_pm[0] == 12:
-                result_pm[0] = 0
-            if result_am[0] == 24:
-                result_am[0] = 12
-            return f"{result_am[0]:02d}:00 to {result_pm[0]:02d}:00"
-        else:
-            result_pm[0]= int(result_pm[0])+12
-            result_am[0] = int(result_am[0])
-            if result_am[0] == 12:
-                result_am[0] = 0
-            if result_pm[0] == 24:
-                result_pm[0] = 12
-            return f"{result_am[0]:02d}:00 to {result_pm[0]:02d}:00"
 
 
 
