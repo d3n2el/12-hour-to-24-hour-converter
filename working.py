6216e7f7f5c5145a487dab64f4e608017880aeb2
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-23 16:40:04 +0200

    Add files via upload

--- working.py
+++ working.py
@@ -14,25 +14,49 @@ def convert(s):
     match_pm = re.search(r"[0-9]{1,2}:?[0-9]{0,2}? (AM|PM)\b", pm, re.IGNORECASE)
     if match_am == None or match_pm == None:
         raise ValueError
-    match_am= match_am.group()
-    match_pm= match_pm.group()
-    find_am= re.search(r"^AM$", match_am)
-    find_pm= re.search(r"^PM$", match_pm)
+    hours_am, minutes_am, period_am = match_am.groups()
+    hours_pm, minutes_pm, period_pm = match_pm.groups()
     if find_am and find_pm:
         results_am= re.split(r"\s", match_am)
         results_pm= re.split(r"\s", match_pm)
         if ":" in match_am:
             hours_am, minutes_am= re.split(r":",results_am[0])
-            final_am = f"{int(hours_am)}:{int(minutes_am)}"
+            hours_am= int(hours_am)
+            minutes_am= int(minutes_am)
+            if hours_am >= 13 or minutes_am >= 60:
+                raise ValueError
+            final_am = f"{hours_am:02d}:{int(minutes_am):02d}"
         else:
-            final_am = int(results_am[0])
+            final_am = f"{int(results_am[0]):02d}"
         if ":" in match_pm:
             hours_pm, minutes_pm = re.split(r":", results_pm[0])
-            final_pm = f"{int(hours_pm)+12}:{int(minutes_pm)}"
+            hours_pm= int(hours_pm)
+            minutes_pm= int(minutes_pm)
+            if minutes_pm >= 60 or hours_pm >= 13:
+                raise ValueError
+            final_pm = f"{hours_pm+12:02d}:{minutes_pm:02d}"
         else:
-            final_pm= int(results_pm[0])+ 12
+            final_pm= f"{int(results_pm[0])+ 12:02d}"
 
-        return f"{final_am:02d}:00 to {final_pm:02d}:00"
+
+        return f"{final_am} to {final_pm}"
+    else:
+        results_am= re.split(r"\s", match_am)
+        results_pm= re.split(r"\s", match_pm)
+        if ":" in match_am:
+            hours_am, minutes_am= re.split(r":",results_am[0])
+            final_am = f"{int(hours_am):02d}:{int(minutes_am):02d}"
+        else:
+            final_am = f"{int(results_am[0])+12:02d}:00"
+        if ":" in match_pm:
+            hours_pm, minutes_pm = re.split(r":", results_pm[0])
+            final_pm = f"{int(hours_pm)+12:02d}:{int(minutes_pm):02d}"
+        else:
+            final_pm= f"{int(results_pm[0]):02d}:00"
+
+        return f"{final_am} to {final_pm}"
+
+
 
 
 
